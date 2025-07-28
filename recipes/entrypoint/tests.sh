383ab64a55508d372d1cc44f0134f10676b31d5e
Matthias Morin <mat@tangoman.io>
2025-07-28 12:56:08 +0200

    Update CI (#1)
    * test(entrypoint): Update test find command
    
    * ci(action): Trigger CI on dev branch

--- recipes/entrypoint/tests.sh
+++ recipes/entrypoint/tests.sh
@@ -2,7 +2,7 @@
 
 ## Run tests
 tests() {
-    find "${test_folder}" -maxdepth 1 -type f -name 'test_*.sh' | sort -t '\0' -n | while read -r FILE
+    find "${test_folder}" -type f -name 'test_*.sh' | sort -t '\0' -n | while read -r FILE
     do
         echo_info "./tests/bash_unit -f tap \"${FILE}\"\n"
         ./tests/bash_unit -f tap "${FILE}"
