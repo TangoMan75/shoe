7206774019d14d83e4bec1d1a3415a725a4be112
Matthias Morin <mat@tangoman.io>
2025-07-28 20:05:33 +0200

    feat: Update _open_in_default_browser

--- src/shell/network/_open_in_default_browser.sh
+++ src/shell/network/_open_in_default_browser.sh
@@ -2,16 +2,17 @@
 
 # Open in default browser
 _open_in_default_browser() {
-    # Synopsis: _open_in_default_browser [IP] [PORT]
-    #   IP:   (optional) Target IP address or domain. (default=127.0.0.1)
-    #   PORT: (optional) Destination port. (default=8080)
+    # Synopsis: _open_in_default_browser [IP] [PORT] [SCHEME]
+    #   IP:     (optional) Target IP address or domain. (default=127.0.0.1)
+    #   PORT:   (optional) Destination port. (default=8080)
+    #   SCHEME: (optional) scheme - e.g. http. (default=http)
 
-    if [ ${#} -gt 2 ]; then echo_danger "error: _open_in_default_browser: too many arguments (${#})\n"; return 1; fi
+    if [ ${#} -gt 3 ]; then echo_danger "error: _open_in_default_browser: too many arguments (${#})\n"; return 1; fi
 
     # set default values
-    set -- "${1:-127.0.0.1}" "${2:-80}"
+    set -- "${1:-127.0.0.1}" "${2:-80}" "${3:-http}"
 
-    echo_info "nohup \"$(_open)\" \"http://$1:$2\" >/dev/null 2>&1\n"
-    nohup "$(_open)" "http://$1:$2" >/dev/null 2>&1
+    echo_info "nohup \"$(_open)\" \"$3://$1:$2\" >/dev/null 2>&1\n"
+    nohup "$(_open)" "$3://$1:$2" >/dev/null 2>&1
 }
 
